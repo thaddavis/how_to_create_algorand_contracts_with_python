6c1fbe0ed85b6f09623414b9896ed58b1125bd91
Your Name <you@example.com>
2022-12-24 00:41:36 -0500

    added auto-reloading support via the integration of nodemon - yes it's a Node.js tool but you can use it for monitoring the files of any type of project for changes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-temperature_in_miami = pd.read_csv("data/temperature_data.csv", index_col=0, parse_dates=True)
+temperature_in_miami = pd.read_csv(
+    "data/temperature_data.csv", index_col=0, parse_dates=True
+)
 temperature_in_miami.head()
 temperature_in_miami.plot(figsize=(10, 6))
 plt.show()
